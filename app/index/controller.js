24ed10d8f0ceb8594d2f3c296615ae5168bb4fa5
rouz majlessi <rmajlessi@gmail.com>
2017-02-17 01:58:18 -0800

    add computed property and observes to test how inpsector works by calling $ E

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isDisabled: true
+  isDisabled: true,
+
+  // value variable/property we set in input box
+  emailAddress: '',
+  // the computed property only changes when you go and use that property
+  actualEmailAddress: Ember.computed('emailAddress', function() {
+    console.log('actualEmailAddress function is called', this.get('emailAddress'));
+  }),
+  // Observers will always be called when the value of emailAddress changes.
+  emailAddressChanged: Ember.observer('emailAddress', function(){
+    console.log('observer is called', this.get('emailAddress'));
+  })
 });
+
+// this are good to check in the console log in the ember inspector by going to routes and clicking on $E this.get('emailAddress') ...........or same for this set.

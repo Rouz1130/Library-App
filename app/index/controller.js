e4d23709f80b0f57b770aa33f4eed8ba87e6ba12
rouz majlessi <rmajlessi@gmail.com>
2017-02-17 02:21:49 -0800

    add isValid to computed property .match

--- app/index/controller.js
+++ app/index/controller.js
@@ -2,17 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-
-  // value variable/property we set in input box
   emailAddress: '',
 
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  
+  isDisabled: Ember.computed.not('isValid')
 
-  // predefined computed property function ember provides. **computed.empty** checks weather property is empy or not
-  isDisabled: Ember.computed.empty('emailAddress')
 });
-
-//BELOW EXAMPLE IS COMPUTED PROPERTY WITHOUT BEING predefined. EXAMPLE SHOWS HOW predefined SAVES TIME
-
-// isDisabled: Ember.computed('emailAddress', function() {
-//     return this.get('emailAddress') === '';
-//   })

ae1a06634f9ff8c7d5ab20b79de3ced00b853259
rouz majlessi <rmajlessi@gmail.com>
2017-02-17 02:04:34 -0800

    add predefined computed property computed.empty

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,18 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isDisabled: true,
+
 
   // value variable/property we set in input box
   emailAddress: '',
-  // the computed property only changes when you go and use that property
-  actualEmailAddress: Ember.computed('emailAddress', function() {
-    console.log('actualEmailAddress function is called', this.get('emailAddress'));
-  }),
-  // Observers will always be called when the value of emailAddress changes.
-  emailAddressChanged: Ember.observer('emailAddress', function(){
-    console.log('observer is called', this.get('emailAddress'));
-  })
+
+
+  // predefined computed property function ember provides. **computed.empty** checks weather property is empy or not
+  isDisabled: Ember.computed.empty('emailAddress')
 });
 
-// this are good to check in the console log in the ember inspector by going to routes and clicking on $E this.get('emailAddress') ...........or same for this set.
+//BELOW EXAMPLE IS COMPUTED PROPERTY WITHOUT BEING predefined. EXAMPLE SHOWS HOW predefined SAVES TIME
+
+// isDisabled: Ember.computed('emailAddress', function() {
+//     return this.get('emailAddress') === '';
+//   })

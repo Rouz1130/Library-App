e25b6db8f23b6d3a3afe70b22546b7e1aea334d4
rouz majlessi <rmajlessi@gmail.com>
2017-02-18 17:12:19 -0800

    refactor controller for index to take out tilda and actually declare var , also create contact page add form with two email labe and send message label, add computed property

---
+++ app/contact/controller.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+
+export default Ember.Controller.extend({
+
+  // refactored took disabled computed property out.  brouht in isValidEmail email add but changed it to isValid for the contact page stays same in index. Added message.length for atleast 5 characters with computed property
+ isValidEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+ isMessageEnoughLong: Ember.computed.gte('message.length', 5),
+
+// combines both abobe computed property
+ isValid: Ember.computed.and('isValidEmail', 'isMessageEnoughLong'),
+
+
+
+  actions: {
+    sendMessage: function() {
+      // setting variables for sendMessage fucntion
+      var email = this.get('emailAddress');
+                  // getter for our declared variables
+      var message = this.get('message');
+
+      alert('Sending your message in progress....');
+
+      // our new variable responseMessage concats email with message
+      var responseMessage = 'To: ' + email + ', Message: ' + message;
+
+      // we now set our variables that we set.
+      // notice emailAddress and message have an empty string that is to allow user input cause we obv dont know what there inputting.
+      this.set('responseMessage', responseMessage);
+      this.set('emailAddress', '');
+      this.set('message');
+    }
+  }
+});
--- app/index/controller.js
+++ app/index/controller.js
@@ -2,10 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  // we can {{yield teh headerMessage}}
+  headerMessage: 'Coming Soon',
+  responseMessage: '',
   emailAddress: '',
 
+
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+
   
-  isDisabled: Ember.computed.not('isValid')
+  actions: {
+    sendInvitation: function() {
+      var email = this.get('emailAddress');
+      alert('Sending in progress... To: ' + email);
+      this.set('responseMessage', "Thank you! We've just sent an email to the following address: " + email);
+      this.set('emailAddress', '');
+    }
+  }
 
 });

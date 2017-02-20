4a26053034c7b01e1b3f0d182c158aa3c6b4dcb4
rouz majlessi <rmajlessi@gmail.com>
2017-02-19 21:53:17 -0800

    add new model for libraies and manually add router for time being for new libraries

--- app/contact/controller.js
+++ app/contact/controller.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  // refactored took disabled computed property out.  brouht in isValidEmail email add but changed it to isValid for the contact page stays same in index. Added message.length for atleast 5 characters with computed property
+  // refactored took disabled computed property out.  brought in isValidEmail email add but changed it to isValid for the contact page stays same in index. Added message.length for atleast 5 characters with computed property
  isValidEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
  // compute.gte is not type its note suppose to be get: stands for greater or = too
  isMessageEnoughLong: Ember.computed.gte('message.length', 5),
@@ -26,7 +26,7 @@ export default Ember.Controller.extend({
       // our new variable responseMessage concats email with message
       var responseMessage = 'To: ' + email + ', Message: ' + message;
 
-      // we now set our variables that we set.
+      // we now set our variables that we get.
       // notice emailAddress and message have an empty string that is to allow user input cause we obv dont know what there inputting.
       this.set('responseMessage', responseMessage);
       this.set('emailAddress', '');
--- app/index/controller.js
+++ app/index/controller.js
@@ -15,6 +15,7 @@ export default Ember.Controller.extend({
   actions: {
     sendInvitation: function() {
       var email = this.get('emailAddress');
+      // this.store.createRecord is letting our server now to save the new email address associated with invitations variable.
       var newInvitation = this.store.createRecord('invitation', { email:email
       });
       // Promise: .save() promise: unique asynchronous feature in javascript. Basically, they’re objects that haven’t completed yet, but are expected to in the future
@@ -22,7 +23,7 @@ export default Ember.Controller.extend({
 
       // .then catch the result of the promise with .then() which is a chained fucntion
 
-    // saving process is successful, ‘fulfilled’ by PROMISE we will get a response from the server.
+    // response => model from server sent to call back as response. the model object contains the ID of our model.
       newInvitation.save().then((response) => {
         // in javascript .this points to the object that wraps around it.
         this.set('responseMessage', "Thank you! We saved your email address with the following id: " + response.get('id'));

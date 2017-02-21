02592dd76db83973aad3c25dccf574412b86f0e9
rouz majlessi <rmajlessi@gmail.com>
2017-02-20 16:16:05 -0800

    add delete action in route.js for library index page and add scss to page

--- app/libraries/index/route.js
+++ app/libraries/index/route.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
   return this.store.findAll('library');
+},
+
+// action buttonin hbs file.
+actions: {
+  deleteLibrary(library) {
+    // message pop up
+    let confirmation = confirm('Are you sure?');
+
+    if (confirmation) {
+      // destroyRecord remove from databse/model
+      library.destroyRecord();
+    }
   }
+}
 });

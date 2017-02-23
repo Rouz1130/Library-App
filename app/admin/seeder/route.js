5570c1f9746b32ac3855b9fcf7f8b5a65b9c77f6
rouz majlessi <rmajlessi@gmail.com>
2017-02-23 01:26:44 -0800

    add seeder file in admin and add RSVP has in seeder route to relate all models

--- app/admin/seeder/route.js
+++ app/admin/seeder/route.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    // RSVP hash to retrive multipile models on same route
+    return Ember.RSVP.hash ){
+      libraries: this.store.findAll('library'),
+      books: this.store.findAll('book'),
+      authors: this.store.findAll('author')
+    })
+  },
+
+  // steupController hook we split up models in their own controller.
+  setupController('controller', model) {
+    controller.set('libraries', model.libraries);
+    controller.set('books', model.books);
+    controller.set('authors', model.authors);
+
+    // route hooks are called in specfic sequence.
+  }
 });

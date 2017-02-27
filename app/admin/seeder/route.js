d4e8126411030ef6b437efc8a919db419b3c9637
rouz majlessi <rmajlessi@gmail.com>
2017-02-27 01:03:33 -0800

    add controller for admin/seeder file and move steupcontroller hook to controller from route

--- app/admin/seeder/route.js
+++ app/admin/seeder/route.js
@@ -1,21 +1,52 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  // You can use these lines to experiment with route hooks.
+  // Uncomment these and comment out the real implementation below.
+  // Open inspection console in your browser and check how Ember call
+  // each hook automatically.
+  //
+  //init() {
+  //  debugger;
+  //},
+  //
+  //beforeModel() {
+  //  debugger;
+  //},
+  //
+  //model() {
+  //  debugger;
+  //},
+  //
+  //afterModel() {
+  //  debugger;
+  //},
+  //
+  //setupController() {
+  //  debugger;
+  //},
+  //
+  //renderTemplate() {
+  //  debugger;
+  //},
+  //
+  //activate() {
+  //  debugger;
+  //}
+
   model() {
-    // RSVP hash to retrive multipile models on same route
-    return Ember.RSVP.hash ({
+    return Ember.RSVP.hash({
       libraries: this.store.findAll('library'),
       books: this.store.findAll('book'),
       authors: this.store.findAll('author')
-    });                          
+    });
   },
 
-  // steupController hook we split up models in their own controller.
-  setupController(controller, model) {
-    controller.set('libraries', model.libraries);
-    controller.set('books', model.books);
-    controller.set('authors', model.authors);
-
-    // route hooks are called in specfic sequence.
-  }
+  // Finally, I just created aliases in the Seeder Controller, so we can comment this out now.
+  // setupController(controller, model) {
+  //   controller.set('libraries', model.libraries);
+  //   controller.set('books', model.books);
+  //   controller.set('authors', model.authors);
+  // }
 });
